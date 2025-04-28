0064d3988ea0ec2824159c55d4f8880bf85aecfa
Karol Kiss <kiss.karol@tct.hu>
2025-04-28 09:00:41 +0200

    Add HomePage component and update index.js to render it
    - Created a new HomePage component in src/HomePage.js that displays a welcome message.
    - Modified src/index.js to render the HomePage component instead of the App component.

---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <header className="HomePage-header">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the homepage of your React application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
